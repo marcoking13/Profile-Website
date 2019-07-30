052d3eeafef0ef7cafec6087b69f9dc795721e69
marcoking13 <marcokhodr16@gmail.com>
2019-07-30 02:09:25 -0700

    made mobile version for current app page

--- src/components/app_current_component.js
+++ src/components/app_current_component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./nav_bar";
+import NavbarMobile from "./mobile_nav_bar";
 class AppCurrent extends React.Component {
   constructor(props){
     super(props);
@@ -27,50 +28,86 @@ class AppCurrent extends React.Component {
     return boxes;
   }
   render(){
-      return (
-        <div className="animator" style={{background:this.props.background,paddingBottom:"10%",backgroundSize:"cover"}}>
-            <div className="navbarAppC">
-                <Navbar changeURL={this.props.changeURL}/>
-            </div>
-            <div className="row1">
-                <div className="cornerBox"style={{background:this.props.background}}>
-                  <img className="cornerLogo" src={this.props.currentApp.image} />
+      if(window.innerWidth >= 590){
+        return (
+          <div className="animator" style={{background:this.props.background,paddingBottom:"10%",backgroundSize:"cover"}}>
+              <div className="navbarAppC">
+                  <Navbar changeURL={this.props.changeURL}/>
                 </div>
+                <div className="row1">
+                  <div className="cornerBox"style={{background:this.props.background}}>
+                    <img className="cornerLogo" src={this.props.currentApp.image} />
+                  </div>
 
-            </div>
+                  </div>
 
-            <div className="row2">
+                  <div className="row2">
 
 
-                <div className="col-6 jumbotron jumbotrons">
-                  <h4 className="titleCurrent">{this.props.currentApp.name}</h4>
-                  <div className="jumboRow1">
+                    <div className="col-6 jumbotron jumbotrons">
+                      <h4 className="titleCurrent">{this.props.currentApp.name}</h4>
+                      <div className="jumboRow1">
                       <h4 className="howToPlay">How To Play</h4>
                       <p className="howToPlay how">
                         Tap as many ghosts as possible before they cover your whole screen!
                       </p>
                   </div>
-                  <div className="jumboRow1">
-                      <h6 className="howToPlay">Coded in:</h6>
-                      <div className="languageBoxer">
-                          {this.renderLanguages()}
+                    <div className="jumboRow1">
+                        <h6 className="howToPlay">Coded in:</h6>
+                        <div className="languageBoxer">
+                            {this.renderLanguages()}
+                        </div>
                       </div>
+                      <div className="jumboRow2">
+                        <h6 className="howToPlay">Complexity:</h6>
+                        <div className="diffBox">
+                          {this.renderDiff()}
+                        </div>
+                      </div>
+
+                      <a href={this.props.currentApp.link}><button onClick = {()=>{this.props.loader()}} className="btn btns  btn-danger">Check it out</button></a>
+
+                    </div>
+                  </div>
+                </div>
+      )
+    }else{
+        return (
+          <div className="container-fluid"style={{background:this.props.background,paddingBottom:"10%",backgroundSize:"cover"}}>
+            <NavbarMobile changeURL = {this.props.changeURL} />
+              <div>
+                <div className="col-12 mlop jumbotron jumbotrons">
+                  <h4 className="titleCurrent ">{this.props.currentApp.name}</h4>
+                  <div className="jumboRow1">
+                  <h4 className="howToPlay">How To Play</h4>
+                  <p className="howToPlay how">
+                    Tap as many ghosts as possible before they cover your whole screen!
+                  </p>
+                </div>
+                <div className="jumboRow1">
+                    <h6 className="howToPlay">Coded in:</h6>
+                    <div className="languageBoxer">
+                      {this.renderLanguages()}
+                    </div>
                   </div>
                   <div className="jumboRow2">
-                      <h6 className="howToPlay">Complexity:</h6>
-                      <div className="diffBox">
-                        {this.renderDiff()}
-                      </div>
+                    <h6 className="howToPlay compM">Complexity:</h6>
+                  <div className="diffBox">
+                      {this.renderDiff()}
+                    </div>
                   </div>
 
                   <a href={this.props.currentApp.link}><button onClick = {()=>{this.props.loader()}} className="btn btns  btn-danger">Check it out</button></a>
 
+                  </div>
+
                 </div>
-            </div>
-        </div>
-      )
+              </div>
+        )
+      }
+    }
   }
-}
+
 
 
 
